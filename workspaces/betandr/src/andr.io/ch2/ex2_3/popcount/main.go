8c60ab54dcda3c01c24605634bb5303ea756fca0
Beth Anderson <beth.anderson@bbc.co.uk>
2018-09-04 20:55:20 +0100

    Ch2 1-4

---
+++ workspaces/betandr/src/andr.io/ch2/ex2_3/popcount/main.go
@@ -0,0 +1,29 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 45.
+
+// Package popcount calculates population count of a uint64
+//!+
+package popcount
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	result := 0
+	for i := uint(0); i < 8; i++ {
+		result += int(pc[byte(x>>(i*8))])
+	}
+
+	return result
+}
+
+//!-
