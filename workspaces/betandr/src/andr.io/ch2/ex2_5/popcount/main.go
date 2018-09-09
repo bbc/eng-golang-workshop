9b1d26acf990a293820c03fe8f0d20b624768a82
Beth Anderson <beth.anderson@bbc.co.uk>
2018-09-09 10:35:32 +0100

    Ch2 Ex4&5

--- workspaces/betandr/src/andr.io/ch2/ex2_5/popcount/main.go
+++ workspaces/betandr/src/andr.io/ch2/ex2_5/popcount/main.go
@@ -37,4 +37,14 @@ func ByShifting(x uint64) int {
 	return result
 }
 
+// ByClearing returns the population count (number of set bits) of x by clearing.
+func ByClearing(x uint64) int {
+	result := 0
+	for x != 0 {
+		x = x & (x - 1) // clear rightmost non-zero bit
+		result++
+	}
+	return result
+}
+
 //!-
