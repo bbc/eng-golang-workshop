8c5263f5fee9ef2e246e0f62a66e5e21a9212702
Beth Anderson <bet@andr.io>
2018-09-17 07:10:47 +0100

    Add 3.7

--- workspaces/betandr/src/andr.io/ch3/ex3_6/main.go
+++ workspaces/betandr/src/andr.io/ch3/ex3_6/main.go
@@ -67,38 +67,3 @@ func mandelbrot(z complex128) color.Color {
 	}
 	return color.Black
 }
-
-//!-
-
-// Some other interesting functions:
-
-func acos(z complex128) color.Color {
-	v := cmplx.Acos(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{192, blue, red}
-}
-
-func sqrt(z complex128) color.Color {
-	v := cmplx.Sqrt(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{128, blue, red}
-}
-
-// f(x) = x^4 - 1
-//
-// z' = z - f(z)/f'(z)
-//    = z - (z^4 - 1) / (4 * z^3)
-//    = z - (z - 1/z^3) / 4
-func newton(z complex128) color.Color {
-	const iterations = 37
-	const contrast = 7
-	for i := uint8(0); i < iterations; i++ {
-		z -= (z - 1/(z*z*z)) / 4
-		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
-			return palette.Plan9[255-contrast*i]
-		}
-	}
-	return color.Black
-}
