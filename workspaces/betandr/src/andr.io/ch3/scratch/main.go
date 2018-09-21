cc3a92b16cdbde37d6c0f985a0fb56ead28b62b4
Beth Anderson <beth.anderson@bbc.co.uk>
2018-09-21 15:26:06 +0100

    Ch3 scratch

--- workspaces/betandr/src/andr.io/ch3/scratch/main.go
+++ workspaces/betandr/src/andr.io/ch3/scratch/main.go
@@ -3,31 +3,48 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	o := 0666
-	fmt.Printf("%d %[1]o %#[1]o\n", o)
-	x := int64(0xdecafbae)
-	fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
+	s := "hello, world"
+	fmt.Println(len(s))
+	fmt.Println(s[0], s[7])
+	fmt.Println(s[:5], s[7:])
+	fmt.Println(s[:])
+	fmt.Println("yo" + s[5:])
 
-	ascii := 'b'
-	unicode := 'シ'
-	newline := '\n'
-	fmt.Printf("%d %[1]c %#[1]q\n", ascii)
-	fmt.Printf("%d %[1]c %#[1]q\n", unicode)
-	fmt.Printf("%d %#[1]q\n", newline)
+	fmt.Print("\a")
 
-	fmt.Printf("%e\n", math.MaxFloat32)
-	fmt.Printf("%e\n", math.MaxFloat64)
-	fmt.Printf("%d\n", math.MinInt32)
-	fmt.Printf("%d\n", math.MinInt64)
+	if 031 == 25 {
+		fmt.Println("Halloween = Christmas")
+	}
 
-	var f float32 = 1 << 24
-	fmt.Println(f == f+1)
+	fmt.Println("hello\nworld")
+	fmt.Println(`hello\nworld`)
 
-	var g float64 = 1 << 24
-	fmt.Println(g == g+1)
+	fmt.Println("\u4e16\u754c")
+
+	// o := 0666
+	// fmt.Printf("%d %[1]o %#[1]o\n", o)
+	// x := int64(0xdecafbae)
+	// fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
+	//
+	// ascii := 'b'
+	// unicode := 'シ'
+	// newline := '\n'
+	// fmt.Printf("%d %[1]c %#[1]q\n", ascii)
+	// fmt.Printf("%d %[1]c %#[1]q\n", unicode)
+	// fmt.Printf("%d %#[1]q\n", newline)
+	//
+	// fmt.Printf("%e\n", math.MaxFloat32)
+	// fmt.Printf("%e\n", math.MaxFloat64)
+	// fmt.Printf("%d\n", math.MinInt32)
+	// fmt.Printf("%d\n", math.MinInt64)
+	//
+	// var f float32 = 1 << 24
+	// fmt.Println(f == f+1)
+	//
+	// var g float64 = 1 << 24
+	// fmt.Println(g == g+1)
 
 }
