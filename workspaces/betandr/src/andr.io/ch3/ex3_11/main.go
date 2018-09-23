4d2caf3f20a4de6f0979f6c4152eee4f72db4192
Beth Anderson <bet@andr.io>
2018-09-23 11:05:20 +0100

    ch3ex311

---
+++ workspaces/betandr/src/andr.io/ch3/ex3_11/main.go
@@ -0,0 +1,42 @@
+// Comma prints its argument numbers with a comma at each power of 1000.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("  %s\n", comma(os.Args[i]))
+	}
+}
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	var buf bytes.Buffer
+
+	point := strings.LastIndex(s, ".")
+	digits := s[point:]
+	s = s[:point]
+
+	if strings.HasPrefix(s, "-") {
+		buf.WriteString("-")
+		s = s[1:]
+	}
+
+	if len(s) < 4 {
+		return s
+	}
+
+	idx := len(s) % 3
+	buf.WriteString(s[:idx])
+
+	for i := idx; i < len(s); i += 3 {
+		buf.WriteString("," + s[i:i+3])
+	}
+
+	return buf.String() + digits
+}
