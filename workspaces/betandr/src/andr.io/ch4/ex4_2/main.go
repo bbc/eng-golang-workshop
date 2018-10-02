af7e69d1617ecdb5a8865fe8d8b1cab88bdb2b16
Beth Anderson <beth.anderson@bbc.co.uk>
2018-10-02 07:03:56 +0100

    Add ch4ex42

---
+++ workspaces/betandr/src/andr.io/ch4/ex4_2/main.go
@@ -0,0 +1,7 @@
+// Write a program that prints the SHA256 hash of it standard input by default
+// but supports a command-line flag to print the SHA384 or SHA512 hash instead.
+package main
+
+func main() {
+
+}
