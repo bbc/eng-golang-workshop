942d1bb2270f02488a294a0e8af551773d6c85f6
Beth Anderson <beth.anderson@bbc.co.uk>
2018-11-16 15:55:31 +0000

    ex44

--- workspaces/betandr/src/andr.io/ch4/ex4_4/main.go
+++ workspaces/betandr/src/andr.io/ch4/ex4_4/main.go
@@ -4,8 +4,12 @@ package main
 
 import "fmt"
 
+// Rotates a slice of integers `s` at the position `pos` with a single pass
+// through the array.
+// (NB: This currently doesn't work properly for odd collections)
 func rotate(pos int, s []int) {
-	for i, j, k := len(s)-1, 3, len(s)-pos; i > 0; i, j = i-1, j-1 {
+	k := len(s) - pos
+	for i, j := len(s)-1, pos; i > 1; i, j = i-1, j-1 {
 		if j <= 0 {
 			j = pos
 			k = k - pos
@@ -18,6 +22,6 @@ func rotate(pos int, s []int) {
 
 func main() {
 	d := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	rotate(3, d[:])
+	rotate(4, d[:])
 	fmt.Println(d)
 }
