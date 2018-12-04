7483305ce92ddcf1a0890adc4e47cc4cbdeae916
Beth Anderson <beth.anderson@bbc.co.uk>
2018-12-04 18:13:21 +0000

    ex4_11

---
+++ workspaces/betandr/src/andr.io/ch4/ex4_11/main.go
@@ -0,0 +1,8 @@
+// Build a tool that lets users create, read, update, and close GitHub issues
+// from the command line, invoking their preferred text editor when substantial
+// text input is required.
+package main
+
+func main() {
+
+}
