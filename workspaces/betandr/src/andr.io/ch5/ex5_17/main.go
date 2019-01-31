a457bc4b76b1b0e4c51b972dae8ced5ae896ae91
Beth Anderson <beth.anderson@bbc.co.uk>
2019-01-31 07:35:38 +0000

    ex5_17

--- workspaces/betandr/src/andr.io/ch5/ex5_17/main.go
+++ workspaces/betandr/src/andr.io/ch5/ex5_17/main.go
@@ -6,6 +6,76 @@
 //     `headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")``
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"golang.org/x/net/html"
+)
+
+// Run with either a list of URLs, such as `element title https://foo.com https://bar.com`
+// or with standard input such as `cat index.html | element title`
+func main() {
+	if len(os.Args) > 1 {
+		for _, url := range os.Args[2:] {
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Fatalf("get %s failed: %s", url, err)
+			}
+			defer resp.Body.Close()
+
+			doc, err := html.Parse(resp.Body)
+			if err != nil {
+				log.Fatalf("parse failed: %s", err)
+			}
+
+			images := ElementsByTagName(doc, "img")
+			for image := range images {
+				fmt.Printf("found: %v\n", image)
+			}
+
+			headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
+			for heading := range headings {
+				fmt.Printf("found: %v\n", heading)
+			}
+		}
+	} else {
+		doc, err := html.Parse(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+			os.Exit(1)
+		}
+		images := ElementsByTagName(doc, "img")
+		for image := range images {
+			fmt.Printf("found: %v\n", image)
+		}
+
+		headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
+		for heading := range headings {
+			fmt.Printf("found: %v\n", heading)
+		}
+	}
+}
+
+// ElementsByTagName returns all the elements of an an HTML node tree that match
+// one of zero or more names given.
+func ElementsByTagName(doc *html.Node, name ...string) []*html.Node {
+	return visit([]*html.Node{}, doc, name...)
+}
+
+// TODO:
+func visit(nodes []*html.Node, n *html.Node, id ...string) []*html.Node {
+	if n.Type == html.ElementNode {
+		if n.Data == "img" {
+
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		nodes = visit(nodes, c, id...)
+	}
+
+	return nodes
 }
