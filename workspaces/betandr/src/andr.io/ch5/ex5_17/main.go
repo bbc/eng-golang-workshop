db1f72954ce6e83b5b367961e96f13c49122e1b7
Beth Anderson <beth.anderson@bbc.co.uk>
2019-02-01 12:38:09 +0000

    ex5_17

--- workspaces/betandr/src/andr.io/ch5/ex5_17/main.go
+++ workspaces/betandr/src/andr.io/ch5/ex5_17/main.go
@@ -7,7 +7,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -15,66 +17,68 @@ import (
 	"golang.org/x/net/html"
 )
 
-// Run with either a list of URLs, such as `element title https://foo.com https://bar.com`
-// or with standard input such as `cat index.html | element title`
+// Run with a list of URLs, such as `ebtn https://foo.com https://bar.com`
 func main() {
-	if len(os.Args) > 1 {
-		for _, url := range os.Args[2:] {
-			resp, err := http.Get(url)
-			if err != nil {
-				log.Fatalf("get %s failed: %s", url, err)
-			}
-			defer resp.Body.Close()
-
-			doc, err := html.Parse(resp.Body)
-			if err != nil {
-				log.Fatalf("parse failed: %s", err)
-			}
-
-			images := ElementsByTagName(doc, "img")
-			for image := range images {
-				fmt.Printf("found: %v\n", image)
-			}
-
-			headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
-			for heading := range headings {
-				fmt.Printf("found: %v\n", heading)
-			}
-		}
-	} else {
-		doc, err := html.Parse(os.Stdin)
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
-			os.Exit(1)
+			log.Fatalf("get %s failed: %s", url, err)
 		}
-		images := ElementsByTagName(doc, "img")
-		for image := range images {
-			fmt.Printf("found: %v\n", image)
+		defer resp.Body.Close()
+
+		doc, err := html.Parse(resp.Body)
+		if err != nil {
+			log.Fatalf("parse failed: %s", err)
 		}
 
+		// find image tags
+		images := ElementsByTagName(doc, "img")
+		fmt.Printf("found %d images\n", len(images))
+		for _, image := range images {
+			fmt.Printf("%v\n", renderNode(image))
+		}
+
+		// find heading tags
 		headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
-		for heading := range headings {
-			fmt.Printf("found: %v\n", heading)
+		fmt.Printf("found %d headings\n", len(headings))
+		for _, heading := range headings {
+			fmt.Printf("%v\n", renderNode(heading))
 		}
 	}
+}
+
+// renderNode returns a HTML string representation of the `html.Node` supplied
+func renderNode(n *html.Node) string {
+	var buf bytes.Buffer
+	w := io.Writer(&buf)
+	html.Render(w, n)
+	return buf.String()
 }
 
 // ElementsByTagName returns all the elements of an an HTML node tree that match
 // one of zero or more names given.
 func ElementsByTagName(doc *html.Node, name ...string) []*html.Node {
-	return visit([]*html.Node{}, doc, name...)
+	var ids = make(map[string]bool)
+	for _, n := range name { // turn our slice of strings into a map for speed
+		ids[n] = true
+	}
+
+	return visit([]*html.Node{}, doc, ids)
 }
 
-// TODO:
-func visit(nodes []*html.Node, n *html.Node, id ...string) []*html.Node {
-	if n.Type == html.ElementNode {
-		if n.Data == "img" {
+// visit checks if the current node matches any supplied `ids` then adds it to
+// nodes slice which is passed back into the visit function as it traverses the
+// `n` node tree.
+func visit(nodes []*html.Node, n *html.Node, ids map[string]bool) []*html.Node {
 
+	if n.Type == html.ElementNode {
+		if ids[n.Data] {
+			nodes = append(nodes, n)
 		}
 	}
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		nodes = visit(nodes, c, id...)
+		nodes = visit(nodes, c, ids)
 	}
 
 	return nodes
