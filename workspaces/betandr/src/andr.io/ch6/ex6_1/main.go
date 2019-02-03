a5f5ffa309d9e826542cd9b151debc8df8049d38
Beth Anderson <beth.anderson@bbc.co.uk>
2019-02-03 18:04:15 +0000

    ex6_1

---
+++ workspaces/betandr/src/andr.io/ch6/ex6_1/main.go
@@ -0,0 +1,104 @@
+// Implement these additional methods:
+//  `func (*IntSet) Len() int      // return the number of elements`
+//  `func (*IntSet) Remove(x int)  // remove x from the set`
+//  `func (*IntSet) Clear()        // remove all elements from the set`
+//  `func (*IntSet) Copy() *IntSet // return a copy of the set`
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// Len returns the number of elements
+func (*IntSet) Len() int {
+	// TODO
+	return 0
+}
+
+// Remove removes `x` from the set
+func (*IntSet) Remove(x int) {
+	// TODO
+}
+
+// Clear removes all elements from the set
+func (*IntSet) Clear() {
+	// TODO
+}
+
+// Copy returns a copy of the set
+func (*IntSet) Copy() *IntSet {
+	// TODO
+	return nil
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+// TODO re-write using strings.Builder?
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func main() {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+
+	fmt.Println(x.Has(9), x.Has(123))
+}
