9d09fcebd7838a48e229fcb4f49d2a8f6fe3fbc5
Beth Anderson <beth.anderson@bbc.co.uk>
2019-02-28 11:02:17 +0000

    update ex4_2 to use stdin

--- workspaces/betandr/src/andr.io/ch4/ex4_2/main.go
+++ workspaces/betandr/src/andr.io/ch4/ex4_2/main.go
@@ -7,24 +7,29 @@ import (
 	"crypto/sha512"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
-func main() {
+var shaType = flag.String("a", "sha256", "The algorithm to use: sha256/sha384/sha512")
 
-	shaType := flag.String("a", "sha256", "The algorithm to use: sha256/sha384/sha512")
-	s := flag.String("s", "", "The string to hash.")
+func main() {
 	flag.Parse()
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	}
 
 	if *shaType == "sha256" {
-		fmt.Printf("%x\n", sha256.Sum256([]byte(*s)))
+		fmt.Printf("%x\n", sha256.Sum256(input))
 
 	} else if *shaType == "sha384" {
-		fmt.Printf("%x\n", sha512.Sum384([]byte(*s)))
+		fmt.Printf("%x\n", sha512.Sum384(input))
 
 	} else if *shaType == "sha512" {
-		fmt.Printf("%x\n", sha512.Sum512([]byte(*s)))
+		fmt.Printf("%x\n", sha512.Sum512(input))
 
 	} else {
-		fmt.Println("unknown algorithm %s", *shaType)
+		fmt.Printf("unknown algorithm: %s\n", *shaType)
 	}
 }
