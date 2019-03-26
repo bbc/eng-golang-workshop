8a37d581734c16b36881e438a52f2dfab153db78
babbc <beth.anderson@bbc.co.uk>
2019-03-26 16:42:15 +0000

    ex7_14

---
+++ workspaces/betandr/src/andr.io/ch7/ex7_14/eval/min_test.go
@@ -0,0 +1,31 @@
+package eval
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMin(t *testing.T) {
+	tests := []struct {
+		expr string
+		env  Env
+		want string
+	}{
+		{"min(x, y)", Env{"x": 3, "y": 4}, "3"},
+		{"min(y, x)", Env{"x": 5, "y": 4}, "4"},
+		{"min(x, x)", Env{"x": 0}, "0"},
+	}
+	for _, test := range tests {
+		expr, err := Parse(test.expr)
+		if err != nil {
+			t.Error(err) // parse error
+			continue
+		}
+		got := fmt.Sprintf("%.6g", expr.Eval(test.env))
+		fmt.Printf("\t%v => %s\n", test.env, got)
+		if got != test.want {
+			t.Errorf("%s.Eval() in %v = %q, want %q\n",
+				test.expr, test.env, got, test.want)
+		}
+	}
+}
